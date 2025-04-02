13d3de42f0113097cbf90291bb14c5677b7b553d
OlegB <oleg89115498683@yandex.ru>
2025-04-02 20:14:21 +0300

    Закончмил

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
-    public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
-    }
+  public static void main(String[] args) {
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+    System.out.println(bean.getMessage());
+    HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+    System.out.println(bean2.getMessage());
+
+    System.out.println(bean == bean2);
+
+    Cat one = applicationContext.getBean(Cat.class);
+    System.out.println(one);
+    Cat two = applicationContext.getBean(Cat.class);
+    System.out.println(two);
+
+    System.out.println(one == two);
+  }
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,14 +1,12 @@
 public class HelloWorld {
- 
-    private String message;
- 
-    public String getMessage() {
-        return message;
-    }
- 
-    public void setMessage(String message) {
-        this.message = message;
-    }
-     
+
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
 }
-//fafafafsa[fv]
